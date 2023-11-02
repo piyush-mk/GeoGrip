948f41a1a8bcd4b1d0042a4893f50bdab548399d
Piyush Mohapatra <piyush.ofcl@gmail.com>
2023-11-02 15:22:17 +0530

    create app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+import time
+import os
+import pathlib, platform
+import pprint as pp
+import shutil
+from io import BytesIO
+from os.path import basename, join
+import timm
+
+from natsort import natsorted
+import skimage
+import streamlit as st
+from fastai.vision.all import PILImage, load_learner, Image, platform, requests
+import timm
+from natsort import natsorted
+from skimage import io
+from skimage.transform import resize
+
